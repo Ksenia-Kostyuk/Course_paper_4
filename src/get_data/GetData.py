5860ae17202e01441523770f472e29516dfbb4a4
Ksenia-Kostyuk <hatan2235@mail.ru>
2024-03-15 11:12:22 +0300

    get list vacancy

--- src/get_data/GetData.py
+++ src/get_data/GetData.py
@@ -20,10 +20,23 @@ class GetData(AbctrGetData):
     """Подключается к серверу поиска вакансий"""
 
     def connection_api(self):
+        """
+        Осуществляет подключение к сервису
+        :return:(int) статус подключения
+        """
         response = requests.get("https://api.hh.ru/vacancies")
         return response.status_code
 
-    def getting_vacations(self):
-        response = requests.get("https://api.hh.ru/vacancies")
-        return response.json().get("items")
-
+    def getting_vacations(self, param: str):
+        """
+        Возвращает вакансии с удаленного сервиса
+        если параметр не передан, осуществляет посик по умолчанию
+        :param param: (str) Параметр поиска вакансий
+        :return: (str) файл json
+        """
+        if param:
+            response = requests.get("https://api.hh.ru/vacancies", params = {"text": param})
+            return response.json().get("items")
+        else:
+            response = requests.get("https://api.hh.ru/vacancies")
+            return response.json().get("items")
